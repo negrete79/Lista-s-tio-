36e49e7ca054365bce749b7ab6bbba126c8b5067
negrete79 <116903139+negrete79@users.noreply.github.com>
2026-09-01 11:39:16 -0300

    Update service worker cache version and assets

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME='recanto-limeira-v1';
-const ASSETS=['./','./index.html','./manifest.json','./service-worker.js','./html2pdf.bundle.min.js','./icon-192.png','./icon-512.png'];
-self.addEventListener('install', e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
-self.addEventListener('activate', e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE_NAME).map(x=>caches.delete(x)))));self.clients.claim();});
-self.addEventListener('fetch', e=>{e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).catch(()=>e.request.destination==='document'?caches.match('./index.html'):null)))});
+const CACHE='recanto-limeira-v5';
+const FILES=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('./index.html'))));});
